d60e20dd14f23bc3eec112df4e622d7c4bbaa13c
Dominique Feyer <dfeyer@ttree.ch>
2017-10-04 18:25:37 +0200

    TASK: Code cleanup

--- Resources/Private/GeoPointEditor/src/GeoPointEditor.js
+++ Resources/Private/GeoPointEditor/src/GeoPointEditor.js
@@ -1,61 +1,62 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
-import { Button, Icon, Label } from '@neos-project/react-ui-components';
+import {Button, Icon, Label} from '@neos-project/react-ui-components';
 import I18n from '@neos-project/neos-ui-i18n';
-import { neos } from '@neos-project/neos-ui-decorators';
+import {neos} from '@neos-project/neos-ui-decorators';
 
 const SecondaryEditor = 'Ttree.GeoPointEditor/Inspector/Secondary/Editors/GeoPointEditorWrapper';
 
 @neos(globalRegistry => ({
-	secondaryEditorsRegistry: globalRegistry.get('inspector').get('secondaryEditors')
+    secondaryEditorsRegistry: globalRegistry.get('inspector').get('secondaryEditors')
 }))
 export default class GeoPointEditor extends PureComponent {
 
-	static propTypes = {
-		identifier: PropTypes.string.isRequired,
-		label: PropTypes.string.isRequired,
-		value: PropTypes.string,
-		commit: PropTypes.func.isRequired,
-	};
+    static propTypes = {
+        identifier: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        value: PropTypes.string,
+        commit: PropTypes.func.isRequired,
+        renderSecondaryInspector: PropTypes.func.isRequired,
+        secondaryEditorsRegistry: PropTypes.func.isRequired
+    };
 
-	constructor(props) {
-		super(props);
-		this.handleOpenMap = this.handleOpenMap.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.handleOpenMap = this.handleOpenMap.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-	render() {
-		const { label, value, identifier } = this.props;
+    render() {
+        const {label, value, identifier} = this.props;
 
-		console.log('value', value);
+        const {lat = null, lon = null} = value;
 
-		const { lat = null, lon = null } = value ? JSON.parse(value) : {};
+        const currentPosition = lat !== null && lon !== null ? (<div>Current value: {parseFloat(lat).toFixed(2)} / {parseFloat(lon).toFixed(2)}</div>) : null;
 
-		return (
-			<div>
-				<Label htmlFor={identifier}>
-					<Button onClick={this.handleOpenMap} style="brand">
-						<Icon icon="map" padded="right" style="lighter" title="Edit" />
-						<I18n id={label} />
-					</Button>
-				</Label>
-				<div>Current value: {parseFloat(lat).toFixed(2)} / {parseFloat(lon).toFixed(2)}</div>
-			</div>
-		);
-	}
+        return (
+            <div>
+                <Label htmlFor={identifier}>
+                    <Button onClick={this.handleOpenMap} style="brand">
+                        <Icon icon="map" padded="right" style="lighter" title="Edit"/>
+                        <I18n id={label}/>
+                    </Button>
+                </Label>
+                {currentPosition}
+            </div>
+        );
+    }
 
-	handleChange(newValue) {
-		const { commit } = this.props;
-		console.log('newValue', newValue);
-		commit(JSON.stringify(newValue));
-	}
+    handleChange(newValue) {
+        const {commit} = this.props;
+        commit(JSON.stringify(newValue));
+    }
 
-	handleOpenMap() {
-		const { renderSecondaryInspector, secondaryEditorsRegistry, value } = this.props;
-		const { component: GeoPointEditorWrapper } = secondaryEditorsRegistry.get(SecondaryEditor);
-		renderSecondaryInspector('TTREEGEOPOINT_GEOPOINTEDITOR_EDIT', () =>
-			<GeoPointEditorWrapper onChange={this.handleChange} point={value ? JSON.parse(value) : undefined} />
-		);
-	}
+    handleOpenMap() {
+        const {renderSecondaryInspector, secondaryEditorsRegistry, value} = this.props;
+        const {component: GeoPointEditorWrapper} = secondaryEditorsRegistry.get(SecondaryEditor);
+        renderSecondaryInspector('TTREEGEOPOINT_GEOPOINTEDITOR_EDIT', () =>
+            <GeoPointEditorWrapper onChange={this.handleChange} point={value ? JSON.parse(value) : undefined}/>
+        );
+    }
 }
--- Resources/Private/GeoPointEditor/src/GeoPointEditorWrapper.js
+++ Resources/Private/GeoPointEditor/src/GeoPointEditorWrapper.js
@@ -4,38 +4,38 @@ import PropTypes from 'prop-types';
 import GoogleMapLoader from './GoogleMapLoader';
 
 class GeoPointEditorWrapper extends PureComponent {
-	constructor(props) {
-		super(props);
-		this.handleChange = this.handleChange.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-	render() {
-		const {point} = this.props;
-		return <GoogleMapLoader
-			apiKey='AIzaSyDEfdbzA3qBvgkoEH0m374Lb_JSsfSf14E'
-			point={point}
-			onChange={this.handleChange}
-		/>;
-	}
+    render() {
+        const {point} = this.props;
+        return (<GoogleMapLoader
+            apiKey="AIzaSyDEfdbzA3qBvgkoEH0m374Lb_JSsfSf14E"
+            point={point}
+            onChange={this.handleChange}
+            />);
+    }
 
-	handleChange(newValue) {
-		this.props.onChange(newValue);
-	}
+    handleChange(newValue) {
+        this.props.onChange(newValue);
+    }
 }
 
 GeoPointEditorWrapper.propTypes = {
-	onChange: PropTypes.func.isRequired,
-	point: PropTypes.shape({
-		lat: PropTypes.number,
-		lon: PropTypes.number
-	}).isRequired,
+    onChange: PropTypes.func.isRequired,
+    point: PropTypes.shape({
+        lat: PropTypes.number,
+        lon: PropTypes.number
+    }).isRequired
 };
 
 GeoPointEditorWrapper.defaultProps = {
-	point: {
-		lat: -34.397,
-		lon: 150.644
-	}
+    point: {
+        lat: -34.397,
+        lon: 150.644
+    }
 };
 
 export default GeoPointEditorWrapper;
